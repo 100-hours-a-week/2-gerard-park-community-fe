a95e3844c5883ba7cab9ce6c12608cb2cf22bdb3
notedsj <dev.gerardpark@gmail.com>
2025-02-11 20:27:40 +0900

    default profile 2

--- js/board.js
+++ js/board.js
@@ -54,7 +54,7 @@ async function loadPosts() {
                     </div>
                     <hr style="border:1px solid#dbdbdb; height: 1px !important; display: block !important; width: 100% !important;"/>
                     <div class="row_c" style="justify-content: flex-start;">
-                        <img src="${(API_URL+post.profileImage) || '../lib/defaultProfilePic.jpg'}" class="imgProfile" alt="profile">
+                        <img src="${post.profileImage ? (API_URL+post.profileImage) : '../lib/defaultProfilePic.jpg'}" class="imgProfile" alt="profile">
                         <div style="margin-left: 10px;font-weight: 500;font-size: large;">
                             ${post.username}
                         </div>
